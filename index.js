f0c51d59578a06558efd2f436d03e5e290822365
anujaagare <125205774+anujaagare@users.noreply.github.com>
2023-10-02 11:23:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict";
+document.querySelector(".btn").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const inputTempValue = document.querySelector(".inputtemp").value;
+    const inputTempUnit = document.querySelector(".choose").value;
+    const convertToUnit = document.querySelector(".choosen").value;
+
+    const celsiusToFahrenheit = (C) => {
+        return C * (9 / 5) + 32;
+    };
+    const fahrenheitToCelsius = (F) => {
+        return (F - 32) * (5 / 9);
+    };
+    const celsiusToKelvin = (C) => {
+        return  C + 273.15;
+    };
+    const kelvinToCelsius = (K) => {
+        return K - 273.15;
+    };
+
+    let result;
+
+    if (inputTempUnit === "°C" && convertToUnit === "Celsius °C") {
+        result = inputTempValue;
+    }
+    if (inputTempUnit === "°F" && convertToUnit === "Fahrenheit °F") {
+        result = inputTempValue;
+    }
+    if (inputTempUnit === "°K" && convertToUnit === "Kelvin °K") {
+        result = inputTempValue;
+    }
+
+    if (inputTempUnit === "°C" && convertToUnit === "Fahrenheit °F") {
+        result = celsiusToFahrenheit(inputTempValue);
+    }
+    if (inputTempUnit === "°F" && convertToUnit === "Celsius °C") {
+        result = fahrenheitToCelsius(inputTempValue);
+    }
+    if (inputTempUnit === "°C" && convertToUnit === "Kelvin °K") {
+        result = celsiusToKelvin(inputTempValue);
+    }
+    if (inputTempUnit === "°K" && convertToUnit === "Celsius °C") {
+        result = kelvinToCelsius(inputTempValue);
+    }
+    if (inputTempUnit === "°F" && convertToUnit === "Kelvin °K") {
+        result = fahrenheitToCelsius(inputTempValue);
+        result = celsiusToKelvin(result);
+    }
+    if (inputTempUnit === "°K" && convertToUnit === "Fahrenheit °F") {
+        result = kelvinToCelsius(inputTempValue);
+        result = celsiusToFahrenheit(result);
+    }
+
+    document.querySelector(".result").textContent = `${result % 1 === 0 ? result : result.toFixed(2)
+        } ${convertToUnit.slice(-2)}`;
+})
